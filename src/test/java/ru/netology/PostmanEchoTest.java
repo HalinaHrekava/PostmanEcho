f952ee5343ab4a8b74596b37119e92b440973f78
Halina Hrekava <gluk2019@mail.ru>
2022-09-07 15:43:20 +0300

    Test fixed

--- src/test/java/ru/netology/PostmanEchoTest.java
+++ src/test/java/ru/netology/PostmanEchoTest.java
@@ -13,14 +13,14 @@ public class PostmanEchoTest {
 // Предусловия
         given()
                 .baseUri("https://postman-echo.com")
-                .body("Postman") // отправляемые данные (заголовки и query можно выставлять аналогично)
+                .body("some data") // отправляемые данные (заголовки и query можно выставлять аналогично)
 // Выполняемые действия
                 .when()
                 .post("/post")
 // Проверки
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("Postman"));
+                .body("data", equalTo("some data"));
 
     }
 }
